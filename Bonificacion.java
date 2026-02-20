83f920a3461f7afd33ea7fe24484fcfb0d83eeb3
alejamc14 <alejandra.madrid1426@gmail.com>
2026-02-20 09:00:05 -0500

    Segundo Commit

---
+++ Bonificacion.java
@@ -0,0 +1,44 @@
+public class Bonificacion {
+       public static void main(String[] args) {
+
+        //Declaración e inicialización
+
+     int a = 10;
+     int b = 5;
+     int c = -3;
+     int d = 20;
+
+     double e = 2.5;
+     double f = -1.5;
+     double g = 4.8;
+     double h = 0.5;
+
+     float i = 3.2f;
+     float j = -7.4f;
+
+     long k = 1000;
+     long l = 50000;
+
+     short m = 2;
+     byte n = 1;
+
+     boolean q = true;
+     boolean r = false;
+
+     double s = 2.80;
+     long t = 100000;
+
+        //Reansignaciones
+
+     a = b + c;
+     e = a * 2;
+     g = e + f;
+     k = a + l;
+     d = a + b;
+
+     System.out.println("a" +a);
+     System.out.println("e" +e);
+     System.out.println("g" +g);
+     System.out.println("k" +k);
+    }
+}
